6bc6a7761e0dfd70deee91750da3a9c14dd4ad91
ganjangtong-blip <ganjangtong@gmail.com>
2025-09-10 10:23:30 +0900

    202509101022_Init
    깃허브 연동 테스트

---
+++ Assets/GithubTest.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GithubTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //잠온다 
+        /*졸린다
+         진짜 끝나면 잘거야
+       */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
